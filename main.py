cefef92da5aea4d38765f3e093f3f68b640f4757
Devendra Pitam <dp787@njit.edu>
2024-03-25 19:00:03 -0400

    added

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import sys
+import logging
 from calculator.calculator import Calculator
+
+# Configure logging settings
+logging.basicConfig(
+    level=logging.DEBUG,  # Set the minimum logging level
+    format='%(asctime)s - %(levelname)s - %(message)s',  # Define the log format
+    filename='app.log',  # Specify the log file name
+    filemode='w'  # Specify the file mode ('w' for write)
+)
 
 def perform_operation(op, num1, num2):
     """Performs a calculation based on the operation provided."""
@@ -22,10 +31,12 @@ def parse_and_validate_numbers(num1, num2):
         num2 = float(num2)
         return num1, num2
     except ValueError:
+        logging.error(f"Invalid number input: {num1} or {num2} is not a valid number.")
         raise ValueError(f"Invalid number input: {num1} or {num2} is not a valid number.")
 
 def main():
     if len(sys.argv) < 4:
+        logging.error("Not enough arguments provided.")
         print("Usage: python main.py <number1> <number2> <operation>")
         print("Operations are 'add', 'subtract', 'multiply', 'divide'")
         return
@@ -35,8 +46,10 @@ def main():
     try:
         num1, num2 = parse_and_validate_numbers(arg_num1, arg_num2)
         result = perform_operation(operation, num1, num2)
+        logging.info(f"Performed operation: {num1} {operation} {num2} = {result}")
         print(f"The result of {num1} {operation} {num2} is equal to {result}")
     except ValueError as e:
+        logging.error(str(e))
         print(e)
 
 if __name__ == "__main__":
